f21c7bd6adb7930387bb6c2017db1510fd7460e8
PoppyCurtis <poppy.curtis@gmail.com>
2019-06-02 11:33:20 +0100

    Track user input in the state via the onChange and onKeyPress methods

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+   state = { artistQuery: '' };
    
+   updateArtistQuery = event => {
+       console.log('event.target.value', event.target.value);
+       this.setState({artistQuery: event.target.value});
+   }
+   
+   searchArtist = () => {
+    console.log('this.state', this.state);
+   }
+
+   handleKeyPress = event => {
+        if (event.key === 'Enter') {
+            this.searchArtist();
+        }
+   }
+
     render() {
         return (
             <div>
               <h2>Music Master</h2>
-              <input placeholder='Search for an Artist'/>
+              <input onChange={this.updateArtistQuery} 
+              onKeyPress={this.handleKeyPress}
+              placeholder='Search for an Artist'/>
+              <button onClick={this.searchArtist}>Search</button>
             </div>
         )
     }
